a3069443db2ad2ddbafdb36462de72723eac31e7
alminoris <olegasaulov.prog@gmail.com>
2025-01-20 10:00:15 +0200

    Backported to 1.18.2

--- src/main/java/net/alminoris/aestheticseating/AestheticSeatingDataGenerator.java
+++ src/main/java/net/alminoris/aestheticseating/AestheticSeatingDataGenerator.java
@@ -8,11 +8,11 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class AestheticSeatingDataGenerator implements DataGeneratorEntrypoint
 {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
-	{
-		fabricDataGenerator.addProvider(ModModelProvider::new);
-		fabricDataGenerator.addProvider(ModRecipeProvider::new);
-		fabricDataGenerator.addProvider(ModLootTableProvider::new);
-	}
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
+    {
+        fabricDataGenerator.addProvider(ModModelProvider::new);
+        fabricDataGenerator.addProvider(ModRecipeProvider::new);
+        fabricDataGenerator.addProvider(ModLootTableProvider::new);
+    }
 }
