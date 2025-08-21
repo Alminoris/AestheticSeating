d7fb3ab8aa6c36528adea47ed2f2af2f4b393a2e
AOA <aoa@d.co>
2025-08-21 09:17:42 +0300

    #0.6.1 Update

--- src/main/java/net/alminoris/aestheticseating/util/helper/BlockSetsHelper.java
+++ src/main/java/net/alminoris/aestheticseating/util/helper/BlockSetsHelper.java
@@ -18,32 +18,49 @@ public class BlockSetsHelper
 
     public static final String[] EXTRA_STONES_WF =
             {
-                    "dolomite_block", "saltmarsh_block"
+                    "dolomite_block", "saltmarsh_block", "loessic_marl_block", "loamy_marl_block", "fossil_marlstone_block"
             };
 
     public static final String[] WOODS =
             {
                     "oak", "birch", "spruce", "jungle", "acacia", "dark_oak",
-                    "crimson", "warped", "mangrove"
+                    "crimson", "warped", "mangrove", "cherry", "bamboo"
             };
 
-    public static final String[] EXTRA_WOODS_AN =
+    public static final String[] AN_WOOD_NAMES =
             {
                     "hazelnut", "hornbeam", "hawthorn", "quince", "plum", "mango", "fig", "viburnum", "white_mulberry", "wild_cherry",
                     "bauhinia", "pine", "fir", "cedar", "araucaria", "juniper"
             };
 
-    public static final String[] EXTRA_WOODS_WF =
+    public static final String[] WF_WOOD_NAMES =
             {
-                    "olive", "tamarisk"
+                    "olive", "tamarisk", "western_serviceberry"
             };
+
+    public static final String[] ST_WOOD_NAMES = new String[] { "walnut", "silver_maple", "staghorn_sumac", "silverberry" };
+
+    public static final String[] WT_WOOD_NAMES = new String[] { "willow", "poplar", "alder", "aspen" };
+
+    public static final String[] MT_WOOD_NAMES = new String[] { "azalea", "apple", "scots_pine", "swamp_oak" };
+
+    public static final String[] NSS_WOOD_NAMES = new String[] { "aspen_nss", "cedar_nss", "coconut_nss", "cypress_nss", "fir_nss", "ghaf_nss",
+            "larch_nss", "mahogany_nss", "maple_nss", "olive_nss", "palo_verde_nss", "redwood_nss", "saxaul_nss", "sugi_nss", "willow_nss", "wisteria_nss" };
 
     public static String[] getWoods()
     {
-        String[] combinedWoods = new String[WOODS.length + EXTRA_WOODS_AN.length + EXTRA_WOODS_WF.length];
+        String[] combinedWoods = new String[WOODS.length + AN_WOOD_NAMES.length + WF_WOOD_NAMES.length+
+                ST_WOOD_NAMES.length+ WT_WOOD_NAMES.length+ MT_WOOD_NAMES.length+ NSS_WOOD_NAMES.length];
         System.arraycopy(WOODS, 0, combinedWoods, 0, WOODS.length);
-        System.arraycopy(EXTRA_WOODS_AN, 0, combinedWoods, WOODS.length, EXTRA_WOODS_AN.length);
-        System.arraycopy(EXTRA_WOODS_WF, 0, combinedWoods, WOODS.length + EXTRA_WOODS_AN.length, EXTRA_WOODS_WF.length);
+        System.arraycopy(AN_WOOD_NAMES, 0, combinedWoods, WOODS.length, AN_WOOD_NAMES.length);
+        System.arraycopy(WF_WOOD_NAMES, 0, combinedWoods, WOODS.length + AN_WOOD_NAMES.length, WF_WOOD_NAMES.length);
+        System.arraycopy(ST_WOOD_NAMES, 0, combinedWoods, WOODS.length + AN_WOOD_NAMES.length+WF_WOOD_NAMES.length, ST_WOOD_NAMES.length);
+        System.arraycopy(WT_WOOD_NAMES, 0, combinedWoods, WOODS.length + AN_WOOD_NAMES.length+WF_WOOD_NAMES.length
+                +ST_WOOD_NAMES.length, WT_WOOD_NAMES.length);
+        System.arraycopy(MT_WOOD_NAMES, 0, combinedWoods, WOODS.length + AN_WOOD_NAMES.length+WF_WOOD_NAMES.length
+                +ST_WOOD_NAMES.length+WT_WOOD_NAMES.length, MT_WOOD_NAMES.length);
+        System.arraycopy(NSS_WOOD_NAMES, 0, combinedWoods, WOODS.length + AN_WOOD_NAMES.length+WF_WOOD_NAMES.length
+                +ST_WOOD_NAMES.length+WT_WOOD_NAMES.length+MT_WOOD_NAMES.length, NSS_WOOD_NAMES.length);
         return combinedWoods;
     }
 
