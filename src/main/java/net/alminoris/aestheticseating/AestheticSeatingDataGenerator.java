bb858844e67a2185811f1b8c9aba24adb4736a65
AOA <aoa@d.co>
2025-10-25 22:16:53 +0300

    #0.6.3 Update

--- src/main/java/net/alminoris/aestheticseating/AestheticSeatingDataGenerator.java
+++ src/main/java/net/alminoris/aestheticseating/AestheticSeatingDataGenerator.java
@@ -1,21 +1,22 @@
 package net.alminoris.aestheticseating;
 
-import net.alminoris.aestheticseating.datagen.ModLootTableProvider;
-import net.alminoris.aestheticseating.datagen.ModModelProvider;
-import net.alminoris.aestheticseating.datagen.ModRecipeProvider;
-import net.alminoris.aestheticseating.item.ModItemGroups;
+import net.alminoris.aestheticseating.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-import java.util.List;
-
 public class AestheticSeatingDataGenerator implements DataGeneratorEntrypoint
 {
-    @Override
-    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
-    {
-        fabricDataGenerator.addProvider(ModModelProvider::new);
-        fabricDataGenerator.addProvider(ModRecipeProvider::new);
-        fabricDataGenerator.addProvider(ModLootTableProvider::new);
-    }
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
+	{
+		fabricDataGenerator.addProvider(ModModelProvider::new);
+		fabricDataGenerator.addProvider(ModRecipeProvider::new);
+		fabricDataGenerator.addProvider(ModLootTableProvider::new);
+		fabricDataGenerator.addProvider(ModLanguageProviderEnUs::new);
+		fabricDataGenerator.addProvider(ModLanguageProviderDeDe::new);
+		fabricDataGenerator.addProvider(ModLanguageProviderEsEs::new);
+		fabricDataGenerator.addProvider(ModLanguageProviderFrFr::new);
+		fabricDataGenerator.addProvider(ModLanguageProviderRuRu::new);
+		fabricDataGenerator.addProvider(ModLanguageProviderUkUa::new);
+	}
 }
